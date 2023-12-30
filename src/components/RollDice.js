9fd27316f5c984d46ef04ce40cd229772b8a6a3e
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-12-30 23:41:32 +0100

    objectkeys

--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -16,7 +16,15 @@ const {dice1,dice2,rolling,totalScore} = state;
 const roll = () => {
  const newDice1 = sides[Math.floor(Math.random() *sides.length)]
  const newDice2 = sides[Math.floor(Math.random() *sides.length)]
-
+console.log(newDice1)
+const score1 = Object.values(newDice1) //[1]
+const score2 = Object.values(newDice2)
+setState({
+  dice1 : Object.keys(newDice1),
+  dice2 : Object.keys(newDice2),
+  rolling:true,
+  totalScore:score1[0] + score2[0]
+})
 }
 
   return (
