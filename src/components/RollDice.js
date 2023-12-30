dd880b89a2c56a477a31bcf02d1bfc44c17d8966
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-12-30 23:23:42 +0100

    rolling totalscore

--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Dice from "./Dice";
 import "./RollDice.css";
 
 function RollDice() {
+  const [state, setState] = useState({
+    dice1: "one",
+    dice2: "three",
+    rolling: false,
+    totalScore: 4,
+  });
+
+const {dice1,dice2,rolling,totalScore} = state;
+
   return (
-  <>
-  <Dice face="one"/>
-  </>
-  )
+    <>
+    <div className="roll-dice">
+      <div className="rollDice-container">
+      <Dice face="one" />
+      <Dice face="three" />
+      </div>
+      <button onClick={roll} disabled={rolling}>
+       {rolling ? "Rolling..." : "Roll Dice"}
+      </button>
+      <h2>Total Score : {totalScore}</h2>
+    </div>
+      <Dice face="one" />
+    </>
+  );
 }
+
+RollDice.defaultProps = {
+ sides:[
+  {one : 1},
+  {two : 2},
+  {three : 3},
+  {four : 4},
+  {five : 5},
+  {six : 6},
+ ]
+}
+
+
 
 export default RollDice;
